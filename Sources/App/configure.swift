dc5dc028f1438883159e7f26d30cddfafa1e4047
Vishwa <71421776+VishwaiOSDev@users.noreply.github.com>
2023-01-28 21:02:46 +0530

    Refactor: Configured to run on port 3001 instead of 8080

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,9 +2,9 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    
+    app.http.server.configuration.port = 3001
+    
     // register routes
     try routes(app)
 }
